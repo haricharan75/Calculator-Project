d28685843fcb18b9e8e5d9dbbce2238d5dff5dab
HariCharan <haricharanadepu1@gmail.com>
2026-07-16 16:22:12 +0530

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,21 @@
+#--------before pytest --cov=. (coverage)----------(one)
+# def add(a, b):
+#     return a + b
+#-------------improve add function---------
+def add(a, b):
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        raise TypeError("Inputs must be numbers")
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
